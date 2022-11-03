c7de6487abf25140e384e1e52f521acfc8e94af1
wll8 <xwdaysme@gmail.com>
2022-11-03 15:06:43 +0800

    fix: 应处理 size 命令的空格路径

--- util.js
+++ util.js
@@ -16,7 +16,7 @@ const defaultArg = {
 }
 
 function treeSize(path = ``){
-  run({note: `分析磁盘空间使用情况`, cmd: `${__dirname}/lib/WizTree/WizTree.exe "${path}"`, code: [0]})
+  run({note: `分析磁盘空间使用情况`, cmd: `"${__dirname}/lib/WizTree/WizTree.exe" "${path}"`, code: [0]})
 }
 
 function handleInfo(path = ``){
